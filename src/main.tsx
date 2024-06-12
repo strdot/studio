c2aa51bfdb79e937c136c6c8422c9da2f668791c
Arvin Wei <clovery.org@gmail.com>
2024-06-12 09:03:05 +0800

    use strdot

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import 'ideanuxt/dist/style.css'
-import ideanuxt from 'ideanuxt'
+import ReactDOM from 'react-dom/client'
+import 'strdot/index.css'
+import { Strdot } from 'strdot'
 
-function main() {
-  import('./extensions/github/github').then(({ github }) => ideanuxt.use(github))
-  import('./extensions/qrcode/qrcode').then(({ qrcode }) => ideanuxt.use(qrcode))
-
-  ideanuxt.create(document.getElementById('root')!)
-}
-
-main()
+ReactDOM.createRoot(document.getElementById('root')!).render(<Strdot />)
